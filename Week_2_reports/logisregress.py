a997eac850944a42e991320ec0388fe6bc43375d
Nguyễn Dương <specialguy368@gmail.com>
2024-11-17 18:04:27 +0700

    17/11/24

--- Week_2_reports/logisregress.py
+++ Week_2_reports/logisregress.py
@@ -3,12 +3,12 @@ import numpy as np
 import matplotlib.pyplot as plt 
 np.random.seed(2)
 
-
+#logistic regression with sigmoid function
 
 X = np.array([[0.50, 0.75, 1.00, 1.25, 1.50, 1.75, 1.75, 2.00, 2.25, 2.50, 
-              2.75, 3.00, 3.25, 3.50, 4.00, 4.25, 4.50, 4.75, 5.00, 5.50]])
-y = np.array([0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1])
-X = np.concatenate((np.ones((1, X.shape[1])), X), axis = 0)    
+              2.75, 3.00, 3.25, 3.50, 4.00, 4.25, 4.50, 4.75, 5.00, 5.50]]) #training_data
+y = np.array([0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1])  #training_labels
+X = np.concatenate((np.ones((1, X.shape[1])), X), axis = 0)    #account bias 
 def sigmoid(s):
     return 1/(1 + np.exp(-s))
 
@@ -23,25 +23,47 @@ def logistic_sigmoid_regression(X, y, w_init, eta, tol = 1e-4, max_count = 10000
         # mix data 
         mix_id = np.random.permutation(N)
         for i in mix_id:
-            xi = X[:, i].reshape(d, 1)
-            yi = y[i]
-            zi = sigmoid(np.dot(w[-1].T, xi))
-            w_new = w[-1] + eta*(yi - zi)*xi
+            xi = X[:, i].reshape(d, 1) # chuyen dang ma tran d hàng, 1 cột
+            yi = y[i] 
+            zi = sigmoid(np.dot(w[-1].T, xi)) # activation fucntion (weight* x)
+            w_new = w[-1] + eta*(yi - zi)*xi  # gradient descent voi ham o ly thuyet
             count += 1
             # stopping criteria
-            if count%check_w_after == 0:                
+            if count%check_w_after == 0:    # check xem da hoi tu chua sau so lan nhat dinh               
                 if np.linalg.norm(w_new - w[-check_w_after]) < tol:
                     return w
             w.append(w_new)
     return w
 eta = .05 
 d = X.shape[0]
-w_init = np.random.randn(d, 1)
+print(d)
+w_init = np.random.randn(d, 1)  # khoi tao trong so ban dau random 
 
 w = logistic_sigmoid_regression(X, y, w_init, eta)
 print(w[-1])
 
 
+
+##plot the data distribution.
+X0 = X[1, np.where(y == 0)][0]
+y0 = y[np.where(y == 0)]
+X1 = X[1, np.where(y == 1)][0]
+y1 = y[np.where(y == 1)]
+
+plt.plot(X0, y0, 'ro', markersize = 8)
+plt.plot(X1, y1, 'bs', markersize = 8)
+
+xx = np.linspace(0, 6, 1000)
+w0 = w[-1][0][0]
+w1 = w[-1][1][0]
+threshold = -w0/w1
+yy = sigmoid(w0 + w1*xx)
+plt.axis([-2, 8, -1, 2])
+plt.plot(xx, yy, 'g-', linewidth = 2)
+plt.plot(threshold, .5, 'y^', markersize = 8)
+plt.xlabel('studying hours')
+plt.ylabel('predicted probability of pass')
+plt.show()
     
 
 
--- Week_3_reports/Kmeans.py
+++ Week_3_reports/Kmeans.py
@@ -6,11 +6,10 @@ np.random.seed(11)
 
 
 # Tao du lieu 
-
-means=[[2,2],[8,3],[3,6]]
+means=[[2,2],[8,3],[3,6]] # các điểm trung tâm ban đầu
 cov=[[1,0],[0,1]]
 N=500 # so diem du lieu
-X0=np.random.multivariate_normal(means[0],cov,N)
+X0=np.random.multivariate_normal(means[0],cov,N) # tạo tập hợp điểm xung quanh tâm 
 X1=np.random.multivariate_normal(means[1],cov,N)
 X2=np.random.multivariate_normal(means[2],cov,N)
 X= np.concatenate((X0,X1,X2),axis=0)
@@ -47,7 +46,7 @@ def kmeans_update_centers(X,labels,K):
     centers=np.zeros((K,X.shape[1]))
     for k in range (K):
         Xk=X[labels==k,:]
-        centers[k,:]=np.mean(Xk,axis=0)
+        centers[k,:]=np.mean(Xk,axis=0)## trung binh toa do Xk
     return centers
 
 def has_converged(centers,new_centers):
@@ -62,16 +61,17 @@ def kmeans(X,K):
     labels=[]
     it=0
     while True:
-        labels.append(kmeans_assign_labels(X,centers[-1]))
-        new_centers=kmeans_update_centers(X,labels[-1],K)
-        if has_converged(centers[-1],new_centers):
+        labels.append(kmeans_assign_labels(X,centers[-1]))##dán nhãn cho các điểm dựa trên tọa độ tâm các cluster
+        new_centers=kmeans_update_centers(X,labels[-1],K) ## update các tâm nhóm dựa trên kết quả vừa chia 
+        if has_converged(centers[-1],new_centers):  ##  nếu hội tụ thì dừng
             break
-        centers.append(new_centers)
+        centers.append(new_centers) # thêm vào lịch sử các tâm 
         it+=1
-    return (centers,labels,it)
+    return (centers,labels,it) # trả về tọa độ tâm, nhãn của từng điểm và số lần thực hiện 
 
 (centers, labels, it) = kmeans(X, K)
 print('Centers found by our algorithm:')
 print(centers[-1])
+print(it)
 
 kmeans_display(X, labels[-1])
